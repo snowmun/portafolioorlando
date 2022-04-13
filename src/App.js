faa848a13d725d017079f1b7c6db21c04d9a435f
snow <smashdojo3@gmail.com>
2022-04-13 11:44:59 -0400

    agregando  basename en route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {ToastContainer} from 'react-toastify';
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL} >
+      <Router >
         <AuthProvider>
           <AppRouter/>
         </AuthProvider>
