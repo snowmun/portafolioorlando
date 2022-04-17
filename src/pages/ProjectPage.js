5ac163c5d327c584d8a9ccc3760b46e94bfc284e
snow <smashdojo3@gmail.com>
2022-04-17 02:10:07 -0400

    se agregaron lso proyectos trabajados

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,8 +1,61 @@
 import React from 'react';
-
+import {Container,Row,Col,Card,Button} from 'react-bootstrap'
+import blackJack from  '../img/proyectos/blackajck.jpg';
+import crudTareas from  '../img/proyectos/crudtareas.jpg';
+import {Link} from 'react-router-dom'
 const ProjectPage =()=> {
   return (
-   <h1>ProjectPage</h1>
+    <Container>
+      <Row className='mt-5'>
+
+        <Col className='col-xs-12 col-sm-12  col-md-9'>
+          <h2>Mis Proyectos </h2>
+          <p>Aqui podras ver los proyectos que he realizado y si gustas puedes entrar a los link 
+            he interactuar con estos, estas pequeñas webs que he realizado para probar distintas herramientas
+          </p>
+        </Col>
+        <Col className='col-xs-12 col-sm-12 col-md-4'>
+          <Card  className='mt-5' style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={blackJack} />
+            <Card.Body>
+              <Card.Title>BlackJack</Card.Title>
+              <Card.Text>
+              Juego del BlackJack o bien conocido como el 21,puedes jugar contra la computadora.
+
+              </Card.Text>
+              <a href="https://snowmun.github.io/blackjackk/" target="_blank"><Button variant="primary">Ir a la Pagina</Button></a>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className='col-xs-12 col-sm-12 col-md-4'>
+          <Card  className='mt-5' style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={crudTareas} />
+            <Card.Body>
+              <Card.Title>Crud Tareas</Card.Title>
+              <Card.Text>
+              Realice este crud con un servidor y cliente, se utiliza node js y en la DB mongoDB, se puede interactuar si gustan. 
+                <br></br>
+                Nota: Como lo monte en heroku al principio se demora un poco
+              </Card.Text>
+              <a href="https://tareafront.herokuapp.com/" target="_blank"><Button variant="primary">Ir a la Pagina</Button></a>
+            </Card.Body>
+          </Card>
+        </Col> 
+        <Col className='col-xs-12 col-sm-12 col-md-4'>
+          <Card  className='mt-5' style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+              <Card.Title>Proyecto Practica</Card.Title>
+                <Card.Text>
+                Este fue le proyecto que realice en mí practica estuve a cargo de todo el backend y el front
+                </Card.Text>
+                <Button variant="primary">Proximamente</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+  </Container>
   );
 }
 
