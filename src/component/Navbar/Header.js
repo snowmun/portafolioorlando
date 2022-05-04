20128990eb212efec574c3d56c2c6bc52de6c9c4
snow <smashdojo3@gmail.com>
2022-05-04 01:42:38 -0400

    se agregaron pestañas  para usuarios logeados

--- src/component/Navbar/Header.js
+++ src/component/Navbar/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect} from 'react';
 import{Navbar,Nav,NavDropdown} from 'react-bootstrap';
 import {NavLink} from 'react-router-dom'
 import routes from '../../route/routes';
@@ -6,7 +6,9 @@ import useAuth from '../auth/useAuth';
 const logo = require ('../../img/logo.png');
 
 const Header =()=> {
+
   const {logout} = useAuth();
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand  as={NavLink} to="/"><img src={logo}        
@@ -21,19 +23,17 @@ const Header =()=> {
           <Nav.Link as={NavLink} to={routes.home}>Inicio</Nav.Link>
 
           <Nav.Link as={NavLink} to={routes.projects}>Proyectos</Nav.Link>
-          {/* <NavDropdown title='Admin'>
-            <NavDropdown.Item  as={NavLink} to={routes.admin.users} >Listar Usuarios</NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to={routes.admin.crearuser}>Crear Usuario</NavDropdown.Item>
+          <Nav.Link  as={NavLink} to={routes.account}>Ver Mi Perfil</Nav.Link>
 
-          </NavDropdown> */}
+          <NavDropdown title='Usuarios'>
+            <NavDropdown.Item  as={NavLink} to={routes.users} >Lista de Usuarios</NavDropdown.Item>
+          </NavDropdown>
         
         </Nav>
         <Nav>
-          {/* <Nav.Link  as={NavLink} to={routes.login}>Iniciar Sesion</Nav.Link>
-          <Nav.Link  as={NavLink} to={routes.register}>Registrarse</Nav.Link> */}
-          <Nav.Link  as={NavLink} to={routes.account}>Ver Mi Perfil</Nav.Link>
-          {/* <Nav.Link  to={routes.account} onClick={logout}>Cerrar Sesion</Nav.Link> */}
-        
+          <Nav.Link  as={NavLink} to={routes.login}>Iniciar Sesion</Nav.Link>
+          <Nav.Link  as={NavLink} to={routes.register}>Registrarse</Nav.Link> 
+          <Nav.Link  to={routes.account} onClick={logout}>Cerrar Sesion</Nav.Link> 
         </Nav>
       </Navbar.Collapse>
   </Navbar>
