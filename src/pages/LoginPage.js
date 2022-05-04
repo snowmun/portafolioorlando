31b5607315826c1d0d757321021814a4ad72230a
snow <smashdojo3@gmail.com>
2022-05-04 01:42:59 -0400

    creacion de login ( desarrollo)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,27 +1,104 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
 import useAuth from '../component/auth/useAuth';
-import { useLocation } from 'react-router-dom';
+import{toast} from 'react-toastify';
+import {Container,Row,Col,Form,Button,Card} from 'react-bootstrap'
+import { Link,useLocation, useNavigate } from 'react-router-dom';
+import './LoginScreen.css';
+const URI='http://localhost:5000/api/v0/login'
 
-const userCredentials ={}
 
 const LoginPage =()=> {
-  const location = useLocation();
-  const {login} = useAuth();
+
+  const [correo, setCorreo] = useState('')
+  const [contrasena, setContrasena] = useState('')   
+  const [user, setUser] = useState(null)   
+  const navigate = useNavigate();
+  
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    if(correo !== '' && contrasena !== ''  ){
+      await axios.post(URI,{correo,contrasena})
+      .then((res) => {
+        const {data} =res;
+          setTimeout(() => {
+            localStorage.setItem('token',data?.token);
+            navigate("/");
+          }, 2500);
+          toast.success('Usuario encontrado con Exito', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme:'dark',
+        }); 
+        }).catch(() => {
+          toast.error('El correo No existe o la contraseña es incorrecta', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme:'dark',
+          });  
+        });
+    }else{
+        toast.error('Debes ingresar los campos email y contraseña', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme:'dark',
+        });           
+    }
+}
+
   return (
-    <div>
-      <h1>Inicio Sesion</h1>
-         <p>Bienvenido a mi Pagina porfavor Logeate con  usuario: invitado  pw:invitado</p>
-         <div>
-          <input type="text" name="" value="" placeholder="nombre de usuario" />
-           
-         </div>
-         <div>
-           <input type="password" name="" value="" placerholder="contraseña"/>
-          
-         </div>
-        
-          <button className="btn btn-primary" onClick={()=>login(userCredentials,location.state?.from)}>Iniciar Sesion</button>
-    </div>
+  <>
+    <Container className="contlogin">
+      <Row>
+        <Col>
+          <Card id='cardlogin' style={{ maxWidth: '500px' }} className=" mx-auto p-2 ">
+            <h3 className='titlelogin'>Inicio Sesion</h3>
+            <Form  onSubmit={handleSubmit} className='formlogin'>
+              <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                <Form.Label column sm="3">
+                  Email
+                </Form.Label>
+                <Col sm="7">
+                  <Form.Control autoComplete="true" onChange={(e)=>setCorreo(e.target.value)}  type="email" placeholder="Ingrese Correo" />
+                </Col> 
+              </Form.Group>
+
+              <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Label column sm="3">
+                  Password
+                </Form.Label>
+                <Col sm="7">
+                  <Form.Control autoComplete="true" onChange={(e)=>setContrasena(e.target.value)} type="password" placeholder="Password" />
+                </Col>
+              </Form.Group>
+              <Form.Group className="ingresarbtn">
+                <Button className="boton mt-2" type="submit" variant="primary" >Iniciar Sesion</Button>
+              </Form.Group>
+              <Form.Group className="linkInicio mt-3">
+                <Link to="/" className="mr-5" > ¿Olvido Su Contraseña? </Link>
+                <Link to="/register" className="ml-5"> Registrarme </Link>
+              </Form.Group>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  </>
 
   );
 }
